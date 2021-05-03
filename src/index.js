45cf9faceacdc0976cd5e6fb95152831159aee6b
Richard <82630326+richardsavala@users.noreply.github.com>
2021-05-03 10:06:12 -0600

    Add files via upload
    adding files manually

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// JSX rules
+// return single element
+// div / section / article / or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+// stateless functional component
+// always return JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Greeting() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+// const Greeting = () => {
+//   return React.createElement(
+//     "h1",
+//     {},
+//     React.createElement("h1", {}, "hello world")
+//   );
+// };
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
