68e12b87fc8d538c495f2b53748b705f10f2e2ac
Richard <82630326+richardsavala@users.noreply.github.com>
2021-05-05 08:06:42 -0600

    Add files via upload
    setup variables in index.js
    added props to const Book
    removed inline styling fro h4
    in index.css styled h4 and p
    added 'align-items: start' to 'media min-widthL768px'

--- src/index.js
+++ src/index.js
@@ -3,47 +3,51 @@ import ReactDom from "react-dom";
 
 // CSS
 import "./index.css";
+// setup vars
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81PcDomicZL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
+  title: "The Giving Tree",
+  author: "Shel Silverstein",
+};
+const secondBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81n1G6Cp7wL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
+  title: "The Bad Seed",
+  author: "Jory John",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+          incidunt eos amet quae eius ex, ipsum earum similique nobis quos!
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author, children }) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/81PcDomicZL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Title = () => <h1>The Giving Tree</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Shel Silverstein
-  </h4>
-);
 
 ReactDom.render(<BookList />, document.getElementById("root"));
