0ea4d430a9cabb2133cf959e588298c9c509c0d1
Richard <82630326+richardsavala@users.noreply.github.com>
2021-05-07 09:22:32 -0600

    Add files via upload
    in index.js 
    added button to Book variable
    added onClick and console.log to h1
    added Book tag to bookList

--- src/index.js
+++ src/index.js
@@ -3,49 +3,54 @@ import ReactDom from "react-dom";
 
 // CSS
 import "./index.css";
-// setup vars
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81PcDomicZL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
-  title: "The Giving Tree",
-  author: "Shel Silverstein",
-};
-const secondBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/81n1G6Cp7wL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
-  title: "The Bad Seed",
-  author: "Jory John",
-};
+const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81PcDomicZL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
+    title: "The Giving Tree",
+    author: "Shel Silverstein",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81n1G6Cp7wL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
+    title: "The Bad Seed",
+    author: "Jory John",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81zhvrYl7yL._UX300__PJprime-reading,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg",
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-          incidunt eos amet quae eius ex, ipsum earum similique nobis quos!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author }) => {
+  // atrtibute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
-      {children}
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
     </article>
   );
 };
